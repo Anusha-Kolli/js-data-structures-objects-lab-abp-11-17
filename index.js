fe4b1a6370d2228a6951693680baec2a9ed144f1
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-12-29 19:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Write your solution in this file!
 const driver = {};
 function updateDriverWithKeyAndValue(driver,key,value){
-  for(key = driver[key]){
-    delete driver[key];
-  }
-  driver.name = 'Sam';
+driver[key]=value;
 
 }
