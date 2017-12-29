ad426ff1bdbf4750464d0736515ceb82fcf3bf68
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-12-29 19:12:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  const driver = {[name:Sam];}
+  const newDriver =Object.assign({},driver);
+  newDriver;
+
+}
